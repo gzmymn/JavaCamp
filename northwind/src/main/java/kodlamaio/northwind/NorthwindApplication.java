5b9ed6b89de388ccb3bbf6d7c133a4490d266aa4
gzmymn <77552353+gzmymn@users.noreply.github.com>
2021-05-28 00:44:40 +0300

    day8

--- northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -2,6 +2,7 @@ package kodlamaio.northwind;
 
 import org.springframework.boot.SpringApplication;
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
